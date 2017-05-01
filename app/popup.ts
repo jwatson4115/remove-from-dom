200a92c599ff749ae356061c385e8d5c2ab1c234
James Watson <jwatson4115@outlook.com>
2017-05-01 22:56:32 +0100

    Commit 2

---
+++ app/popup.ts
@@ -0,0 +1,24 @@
+﻿function deleteDomElement(element : string, amount : number) {
+    chrome.tabs.executeScript(null, {
+        code: "var elementToRemove = '"+element+"'; var deleteAmount = "+amount+";"
+    }, function() {
+        chrome.tabs.executeScript(null, { file: "remove-from-dom.js" });
+    });
+}
+
+window.onload = () => {
+
+    const btnDelete = document.getElementById("delete-button");
+
+    btnDelete.addEventListener("click", () => {
+        const deleteIdentifer = document.getElementById("delete-textbox") as HTMLInputElement;
+        const txtDeleteAmount = document.getElementById("delete-amount") as HTMLInputElement;
+
+        let deleteAmount = Number(txtDeleteAmount.value);
+        deleteAmount = isNaN(deleteAmount) || deleteAmount < 1 ? 0 : deleteAmount;
+
+        deleteDomElement(deleteIdentifer.value, deleteAmount);
+
+    }, false);
+
+};
---
+++ app/remove-from-dom.ts
@@ -0,0 +1,59 @@
+﻿class RemoveFromDom {
+
+    delete(deleteIdentifier: string, amount: number) {
+        const firstCharacter = deleteIdentifier.substr(0, 1);
+
+        if (firstCharacter === "#") {
+            this.deleteById(deleteIdentifier);
+        }
+        else if (firstCharacter === ".") {
+            this.deleteByClass(deleteIdentifier, amount);
+        }
+        else {
+            this.deleteByTag(deleteIdentifier, amount);
+        }
+    }
+
+    private deleteById(idString: string) {
+        idString = this.deleteFirstCharacter(idString);
+        const element = document.getElementById(idString);
+
+        element.parentNode.removeChild(element);
+    }
+
+    private deleteByClass(classString: string, amount: number) {
+        classString = this.deleteFirstCharacter(classString);
+        const elements = document.getElementsByClassName(classString);
+
+        amount = this.getDeleteAmount(amount, elements.length);
+
+        for (let i = 0; i < amount; i++) {
+            elements[0].parentNode.removeChild(elements[0]);
+        }
+    }
+
+    private deleteByTag(tagString: string, amount: number) {
+        const tags = document.getElementsByTagName(tagString);
+        amount = this.getDeleteAmount(amount, tags.length);
+
+        for (let i = 0; i < amount; i++) {
+            tags[0].parentNode.removeChild(tags[0]);
+        }
+    }
+
+    private deleteFirstCharacter(text: string): string {
+        return text.substr(1);
+    }
+
+    private getDeleteAmount(amount: number, elementLength: number): number {
+        if (amount <= 0) {
+            return elementLength;
+        }
+        else {
+            return Math.min(amount, elementLength);
+        }
+    }
+}
+
+const removeFromDom = new RemoveFromDom();
+removeFromDom.delete(elementToRemove, deleteAmount);
